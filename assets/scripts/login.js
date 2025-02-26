790750e4a695fb6bbdbe387d5c264db58ccd1010
Name? What's that? <daniel@fuckyou.zip>
2025-02-26 22:26:09 +0200

    Initial Upload

---
+++ assets/scripts/login.js
@@ -0,0 +1,5 @@
+const button = document.querySelector('.btn');
+button.addEventListener('click', () => {
+    // move to the profile.html page
+    window.location.href = '../../pages/profile.html';
+});
