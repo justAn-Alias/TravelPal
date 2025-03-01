d4bf53044b160e3e7b2a0a1793589ed3d0900c44
Name? What's that? <daniel@fuckyou.zip>
2025-03-01 19:28:52 +0200

    br

--- assets/scripts/login.js
+++
@@ -1,5 +0,0 @@
-const button = document.querySelector('.btn');
-button.addEventListener('click', () => {
-    // move to the profile.html page
-    window.location.href = '../../pages/profile.html';
-});
